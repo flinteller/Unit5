ae5097ec55e3de63ca9a67927ca336a3c210f750
flinteller <flinteller@ssfs.org>
2020-02-26 12:02:52 -0500

    Used total sentiment val method to find star rating

---
+++ src/com/company/ReviewDriver.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.io.FileNotFoundException;
+
+public class ReviewDriver {
+
+    public static void main(String args[]) throws FileNotFoundException {
+        System.out.println(Review.star_val("SimpleReview.txt"));
+    }
+
+}
